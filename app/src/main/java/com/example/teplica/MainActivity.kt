7614052dec0b3705a97c4d27adda4f3a488fa5b4
Max <maksimgt500@gmail.com>
2024-02-15 23:27:46 +0300

    init1

--- app/src/main/java/com/example/teplica/MainActivity.kt
+++ app/src/main/java/com/example/teplica/MainActivity.kt
@@ -1,16 +1,10 @@
 package com.example.teplica
 
 import android.os.Bundle
-import android.renderscript.ScriptGroup.Binding
-import android.util.Log
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.teplica.databinding.ActivityMainBinding
 import okhttp3.OkHttpClient
-import okhttp3.Request
-import okhttp3.WebSocket
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding:ActivityMainBinding
@@ -21,6 +15,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val client: OkHttpClient =  OkHttpClient()
+
+
 
 //        binding.connect.setOnClickListener {
 //            Log.d("PieSocket","Connecting");
